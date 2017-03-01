d37f95ac2ffb1a17378b0de67631cecc9dc49c08
0xKernelG0ru <stoplayin15@gmail.com>
2017-03-01 20:10:24 +0200

    Deleted un-needed files.

--- src/crawler.py
+++ src/crawler.py
@@ -2,8 +2,16 @@ from bs4 import BeautifulSoup
 import parser
 import urllib2
 import urllib
+import json
 
-def anaylze_html(url):
+def grab_static_file(url):
+	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+	headers = {'User-Agent' : user_agent}
+	req = urllib2.Request(url, None, headers)
+	response = urllib2.urlopen(req)
+	html = response.read()
+
+
 
 
 
@@ -16,5 +24,6 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_arguement('html', help='Enter URL of the static file')
 	args = parser.parse_args()
-	content = urllib2.urlopen(args.html)
-	html = content.read()
+	grab_static_file(url)
+
+
